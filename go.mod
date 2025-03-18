88bb2bb8a6cc44edc9dd35c5c6b71b81c4eccb31
tituscarl <titusranario@gmail.com>
2025-03-18 14:59:03 +0800

    add backoff mechanism for subscribe

--- go.mod
+++ go.mod
@@ -4,6 +4,8 @@ go 1.23.6
 
 require (
 	github.com/alphauslabs/pubsub-proto v0.0.2
+	github.com/golang/glog v1.2.4
+	github.com/googleapis/gax-go/v2 v2.14.1
 	google.golang.org/grpc v1.71.0
 )
 
@@ -11,6 +13,7 @@ require (
 	golang.org/x/net v0.34.0 // indirect
 	golang.org/x/sys v0.29.0 // indirect
 	golang.org/x/text v0.21.0 // indirect
+	google.golang.org/api v0.211.0 // indirect
 	google.golang.org/genproto/googleapis/rpc v0.0.0-20250115164207-1a7da9e5054f // indirect
 	google.golang.org/protobuf v1.36.5 // indirect
 )
--- pubsub.go
+++ pubsub.go
@@ -3,9 +3,14 @@ package pubsub
 import (
 	"context"
 	"encoding/json"
+	"time"
 
 	pb "github.com/alphauslabs/pubsub-proto/v1"
+	"github.com/golang/glog"
+	gaxv2 "github.com/googleapis/gax-go/v2"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
 )
 
 func (p *PubSubClient) Close() {
@@ -42,7 +47,7 @@ func (p *PubSubClient) Publish(ctx context.Context, in *PublishRequest) error {
 }
 
 // Subscribes to a subscription. Data will be sent to the Outch, while errors on Errch.
-func (p *PubSubClient) Subscribe(ctx context.Context, in *SubscribeRequest) {
+func Subscribe(ctx context.Context, in *SubscribeRequest) {
 	defer func() {
 		close(in.Errorch)
 		close(in.Outch)
@@ -53,25 +58,58 @@ func (p *PubSubClient) Subscribe(ctx context.Context, in *SubscribeRequest) {
 		Subscription: in.Subcription,
 	}
 
-	stream, err := (*p.clientconn).Subscribe(ctx, req)
-	if err != nil {
-		in.Errorch <- err
-		return
+	do := func() error {
+		conn, err := grpc.Dial("10.146.0.27:50051", grpc.WithInsecure())
+		if err != nil {
+			return err
+		}
+		defer conn.Close()
+
+		clientconn := pb.NewPubSubServiceClient(conn)
+		stream, err := clientconn.Subscribe(ctx, req)
+		if err != nil {
+			return err
+		}
+
+		for {
+			msg, err := stream.Recv()
+			if err != nil {
+				return err
+			}
+			res := &SubscribeResponse{
+				Id:         msg.Id,
+				Payload:    msg.Payload,
+				Attributes: msg.Attributes,
+			}
+			b, _ := json.Marshal(res)
+			in.Outch <- b
+		}
 	}
 
+	bo := gaxv2.Backoff{
+		Initial: 5 * time.Second,
+		Max:     1 * time.Minute,
+	}
+	limit := 30
+	i := 0
 	for {
-		msg, err := stream.Recv()
+		if i >= limit {
+			break
+		}
+		err := do()
 		if err != nil {
+			st, ok := status.FromError(err)
+			if ok {
+				if st.Code() == codes.Unavailable {
+					i++
+					glog.Errorf("Error: %v, retrying in %v, retries left: %v", err, bo.Pause(), limit-i)
+					time.Sleep(bo.Pause())
+					continue
+				}
+			}
 			in.Errorch <- err
-			return
+			break
 		}
-		res := &SubscribeResponse{
-			Id:         msg.Id,
-			Payload:    msg.Payload,
-			Attributes: msg.Attributes,
-		}
-		b, _ := json.Marshal(res)
-		in.Outch <- b
 	}
 }
 
