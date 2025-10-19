4250b62b887d113d23b8196b3ccfb9f4f1b12a4a
tituscarl <titusranario@gmail.com>
2025-10-19 21:13:39 +0800

    ci skip: try decrease max

--- pubsub.go
+++ pubsub.go
@@ -506,8 +506,8 @@ func (p *PubSubClient) SendAckWithRetry(ctx context.Context, id, subscription, t
 	var address string
 
 	bo := gaxv2.Backoff{
-		Initial: 5 * time.Second,
-		Max:     20 * time.Second,
+		Initial: 2 * time.Second,
+		Max:     10 * time.Second,
 	}
 
 	limit := 30
