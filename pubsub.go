61bef16b95246c2d2f65b1bb3cdc8015f3fc4d11
tituscarl <titusranario@gmail.com>
2025-10-29 19:12:45 +0800

    ci skip: don't reset addr

--- pubsub.go
+++ pubsub.go
@@ -538,7 +538,6 @@ func (p *PubSubClient) SendAckWithRetry(ctx context.Context, id, subscription, t
 		if st, ok := status.FromError(err); ok {
 			if st.Code() == codes.Unavailable {
 				sleep := bo.Pause()
-				address = ""
 				p.logger.Printf("Error: %v, retrying in %v, id=%v, sub=%v, retry=%v", err, sleep, id, subscription, i)
 				time.Sleep(sleep)
 				continue
